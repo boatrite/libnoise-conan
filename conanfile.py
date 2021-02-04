14c5ac8b88036bd882f6129c6fb6ba19657c2481
Gwen Boatrite <gwen.boatrite@gmail.com>
2021-02-03 18:02:08 -0600

    Get the libnoise conan package working with example

--- conanfile.py
+++ conanfile.py
@@ -9,5 +9,17 @@ class LibnoiseConan(ConanFile):
     def source(self):
         self.run("git clone git@github.com:qknight/libnoise.git")
 
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="libnoise")
+        cmake.build()
+
     def package(self):
-        self.copy("*.h", dst="include", src="libnoise/src/noise")
+        self.copy("*.h", dst="include", src="libnoise/src")
+        self.copy("*.h", dst="include", src="libnoise/noiseutils")
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["noise", "noiseutils"]
